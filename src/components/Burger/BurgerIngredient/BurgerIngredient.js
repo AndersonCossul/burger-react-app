14395fe0fa8cc9adef653641af29b890dab3b3dd
Anderson Cossul <anderson_cossul@hotmail.com>
2018-01-14 21:00:05 -0200

    Mounted a hard-coded burger.

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -16,26 +16,26 @@ class BurgerIngredient extends Component {
 				)
 				break
 			case ('meat'):
-				ingredient = <div className={classes.Meat}</div>
+				ingredient = <div className={classes.Meat}></div>
 				break;
 			case ('cheese'):
-				ingredient = <div className={classes.Cheese}</div>
+				ingredient = <div className={classes.Cheese}></div>
 				break;
 			case ('salad'):
-				ingredient = <div className={classes.Salad}</div>
+				ingredient = <div className={classes.Salad}></div>
 				break;
 			case ('bacon'):
-				ingredient = <div className={classes.Bacon}</div>
+				ingredient = <div className={classes.Bacon}></div>
 				break;
 			case ('bread-bottom'):
 				ingredient = <div className={classes.BreadBottom}></div>
 				break
 			default:
 				ingredient = null
-		}	
+		}
+		
+		return ingredient
 	}
-
-	return ingredient
 }
 
 BurgerIngredient.propTypes = {
