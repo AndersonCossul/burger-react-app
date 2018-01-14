45cd2c48b81c56c9b2c20742c991d30895a49836
Anderson Cossul <anderson_cossul@hotmail.com>
2018-01-14 21:31:38 -0200

    Build Control.

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React form 'react'
+import classes from './BuildControl.css'
+
+const buildControl = (props) => (
+	<div className={classes.BuildControl}>
+		<div className={classes.Label}>{props.label}</div>
+		<button className={className.Less}>Less</button>
+		<button className={className.More}>More</button>
+	</div>
+)
+
+export default buildControl
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
 	// create an array with the ingredients considering how many times the ingredient repeats
-	const transformedIngredients = Object.keys(props.ingredients)
-										.map(ingredientKey => {
-											return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
-												return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
-											})
-										})
+	let transformedIngredients = Object.keys(props.ingredients)
+		.map(ingredientKey => {
+			return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+				return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
+			})
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el)
+		}, [])
+
+	if(transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients.</p>
+	}
+
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top"/>
