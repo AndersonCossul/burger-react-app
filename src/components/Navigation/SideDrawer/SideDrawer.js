2c2e76c7a2f8218d171dfa09b21a40d77e7c1234
Anderson Cossul <anderson_cossul@hotmail.com>
2018-01-15 21:09:09 -0200

    Closing sidedrawer.

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,28 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Auxi from '../../../hoc/Auxi'
 
 const sideDrawer = (props) => {
+	let attachedClasses = [classes.SideDrawer, classes.Close]
+
+	if (props.show) {
+		attachedClasses = [classes.SideDrawer, classes.Open]
+	}
+
 	return (
-		<div className={classes.SideDrawer}>
-			<div className={classes.Logo}>
-				<Logo/>
+		<Auxi>
+			<Backdrop show={props.show} clicked={props.closed}/>
+			<div className={attachedClasses.join(' ')}>
+				<div className={classes.Logo}>
+					<Logo/>
+				</div>
+				<nav>
+					<NavigationItems/>
+				</nav>
 			</div>
-			<nav>
-				<NavigationItems/>
-			</nav>
-		</div>
+		</Auxi>
 	)
 }
 
