9fe30544e409ebe477ce2fcb8684a168197bb118
Anderson Cossul <anderson_cossul@hotmail.com>
2018-01-14 23:37:34 -0200

    Conditionally showing the modal for order summary.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
 			meat: 0
 		},
 		totalPrice: 4,
-		purchasable: false
+		purchasable: false,
+		purchasing: false
 	}
 
 	updatePurchaseState (ingredients) {
@@ -81,6 +82,10 @@ class BurgerBuilder extends Component {
 		this.updatePurchaseState(updatedIngredients)
 	}
 
+	purchaseHandler = () => {
+		this.setState({purchasing: true})
+	}
+
 	render () {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -93,7 +98,7 @@ class BurgerBuilder extends Component {
 
 		return (
 			<Auxi>
-				<Modal>
+				<Modal show={this.state.purchasing}>
 					<OrderSummary ingredients={this.state.ingredients}/>
 				</Modal>
 				<Burger ingredients={this.state.ingredients}/>
@@ -102,7 +107,8 @@ class BurgerBuilder extends Component {
 					ingredientAdded={this.addIngredientHandler}
 					disabled={disabledInfo}
 					purchasable={this.state.purchasable}
-					price={this.state.totalPrice}/>
+					price={this.state.totalPrice}
+					ordered={this.purchaseHandler}/>
 			</Auxi>
 		)
 	}
