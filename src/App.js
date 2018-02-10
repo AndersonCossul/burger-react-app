0a428e8c3064718c0264acc85ee9768082206c6b
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-10 03:20:04 -0200

    Added routing.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <BrowserRouter>
         <Layout>
-        	<BurgerBuilder/>
-          <Checkout/>
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
         </Layout>
-      </div>
+      </BrowserRouter>
     );
   }
 }
