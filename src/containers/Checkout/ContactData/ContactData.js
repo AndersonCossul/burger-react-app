dccf1887c06defb380bea415e314df4cefb1e8e2
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-12 01:35:25 -0200

    Setting the inputs in the state

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,11 +7,63 @@ import axios from '../../../axios-orders'
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+			name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+			street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+			zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP Code'
+        },
+        value: ''
+      },
+			country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+			email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your E-Mail'
+        },
+        value: ''
+      },
+      deliverMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {
+              value: 'fastest',
+              displayName: 'Fastest'
+            },
+            {
+              value: 'cheapest',
+              displayName: 'Cheapest'
+            }
+          ]
+        },
+        value: ''
+      }
     },
     loading: false
   }
@@ -25,16 +77,7 @@ class ContactData extends Component {
 			ingredients: this.props.ingredients,
 			price: this.props.price, // in a real app, would probably make the server calculate the price, just in case
 			// forced for now
-			customer: {
-				name: 'Anderson Cossul',
-				address: {
-					street: 'Confidential',
-					zipCode: '555555',
-					city: 'Porto Alegre',
-					country: 'Brazil'
-				},
-				email: 'anderson_cossul@hotmail.com'
-			},
+
 			deliverMethod: 'fastest'
 		}
 		axios.post('/orders.json', order)
@@ -45,12 +88,26 @@ class ContactData extends Component {
   }
 
   render () {
+    const formElementsArray = []
+    // "key" will be the name -> name, email, street, zipCode
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      })
+    }
+
     let form = (
       <form onSubmit={(e) => e.preventDefault()}>
-        <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-        <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-        <Input inputtype="input" type="text" name="street" placeholder="Street" />
-        <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+        {
+          formElementsArray.map(formElement => (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}/>
+          ))
+        }
         <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
       </form>
     )
