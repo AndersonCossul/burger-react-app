b2f3ed41bfbcb27a8dbf641c6efac9195d93e6b7
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-14 22:53:56 -0200

    Changing totalPrice when adding and removing ingredients.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,13 +10,6 @@ import Spinner from '../../components/UI/Spinner/Spinner'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 import axios from '../../axios-orders'
 
-const INGREDIENT_PRICES = {
-	salad: 0.5,
-	cheese: 0.4,
-	meat: 1.3,
-	bacon: 0.7
-}
-
 class BurgerBuilder extends Component {
 	state = {
 		purchasable: false, // turns on the order button on true
@@ -48,49 +41,6 @@ class BurgerBuilder extends Component {
 			purchasable: sum > 0
 		})
 	}
-
-	// addIngredientHandler = (type) => {
-	// 	const oldCount = this.state.ingredients[type]
-	// 	const updatedCount = oldCount + 1
-	// 	const updatedIngredients = {
-	// 		...this.state.ingredients
-	// 	}
-	// 	updatedIngredients[type] = updatedCount
-
-	// 	const priceAddition = INGREDIENT_PRICES[type]
-	// 	const oldPrice = this.state.totalPrice
-	// 	const newPrice = oldPrice + priceAddition
-
-	// 	this.setState({
-	// 		ingredients: updatedIngredients,
-	// 		totalPrice: newPrice
-	// 	})
-
-	// 	this.updatePurchaseState(updatedIngredients)
-	// }
-
-	// removeIngredientHandler = (type) => {
-	// 	const oldCount = this.state.ingredients[type]
-	// 	if (oldCount <= 0) {
-	// 		return
-	// 	}
-	// 	const updatedCount = oldCount - 1
-	// 	const updatedIngredients = {
-	// 		...this.state.ingredients
-	// 	}
-	// 	updatedIngredients[type] = updatedCount
-
-	// 	const priceDeduction = INGREDIENT_PRICES[type]
-	// 	const oldPrice = this.state.totalPrice
-	// 	const newPrice = oldPrice - priceDeduction
-
-	// 	this.setState({
-	// 		ingredients: updatedIngredients,
-	// 		totalPrice: newPrice
-	// 	})
-
-	// 	this.updatePurchaseState(updatedIngredients)
-	// }
 
 	purchaseHandler = () => {
 		this.setState({purchasing: true})
